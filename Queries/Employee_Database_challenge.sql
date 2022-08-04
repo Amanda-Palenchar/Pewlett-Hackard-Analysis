6b82cb9e02218ccf60156bef50f51b4909981778
Amanda Palenchar <amanda.palenchar@gmail.com>
2022-08-04 10:59:33 -0600

    Deliverable number 2 complete.

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,3 +1,4 @@
+-- Deliverable 1
 -- Create initial table 
 SELECT e.emp_no,
 	e.first_name,
@@ -22,6 +23,19 @@ FROM retirement_titles
 WHERE to_date = ('9999-01-01')
 ORDER BY emp_no, to_date DESC;
 
+-- Need to change table name
+DROP TABLE most_recent_titles;
+
+-- Renaming table above to match challenge name
+SELECT DISTINCT ON (emp_no) emp_no,
+	first_name,
+	last_name,
+	title
+INTO unique_titles
+FROM retirement_titles
+WHERE to_date = ('9999-01-01')
+ORDER BY emp_no, to_date DESC;
+
 -- Retrieve the number of employees by most recent job title -- accidentally made errors
 SELECT COUNT (title) emp_no,
 	title
@@ -39,3 +53,22 @@ INTO retiring_titles
 FROM most_recent_titles
 GROUP BY title
 ORDER BY emp_no DESC;
+
+-- Deliverable 2
+-- Determining eligibility for the mentorship program
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO menotorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS ti
+ON (e.emp_no = ti.emp_no)
+WHERE de.to_date = ('9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
